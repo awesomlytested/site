95c7d2868d1b9bae829fb33d3fcddd04d9a14e66
Davert <davert.ua@gmail.com>
2022-01-10 02:24:11 +0200

    loading from list from fs

--- pages/index.vue
+++ pages/index.vue
@@ -208,6 +208,7 @@ export default {
   asyncData({ $axios, params }) {
     if (process.static) {
       return import('@/static/data/list.json').then((data) => {
+        console.log(data);
         return {
           data,
           list: data.slice(0, 32).filter(d => !!d.repo),
